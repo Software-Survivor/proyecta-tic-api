bfa3a915c73a91ddaa44439b9dc5f48c75cd5f5c
gustavo <bocettodigital@gmail.com>
2021-11-16 22:11:04 -0500

    Se limpiaron las carpetas ya que habia hecho un merge con mi rama, pero quedo lista la conexion a la base de datos

--- index.ts
+++ index.ts
@@ -1,19 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { ApolloServer } from "apollo-server-express";
 import dotenv from "dotenv";
 import ConectBD from "./db/db";
-import { typeDefs } from "./graphql/type";
-import { resolvers } from "./graphql/resolvers"
 
 
 dotenv.config();
-
-const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers,
-
-});
 
 const app = express();
 
@@ -21,8 +12,6 @@ app.use(express.json());
 app.use(cors());
 app.listen({ port: process.env.PORT || 4000 }, async () => {
     await ConectBD();
-    await server.start();
-    server.applyMiddleware({ app });
     console.log("¡ Servidor encendido correctamente !")
     
 });
