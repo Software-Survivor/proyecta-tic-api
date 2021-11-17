3ee3be708285d250fe2be14ca3bf1bde4cd7ecb9
paulaarambula <arambula.paula@gmail.com>
2021-11-16 21:03:47 -0500

    actualización enums

--- models/enums.ts
+++ models/enums.ts
@@ -1,30 +1,43 @@
 enum Enum_Rol {
-    estudiante = 'Estudiante',
-    lider = 'Líder',
-    administrador = 'Administrador',
+    estudiante = "Estudiante",
+    lider = "Líder",
+    administrador = "Administrador",
+} 
+
+
+enum Enum_StatusUsers {
+    pendiente = "Pendiente",
+    autorizado = "Autorizado",
+    no_autorizado = "No autorizado",
+
 }
 
-enum Enum_EstadoUsuario {
-    pendiente = 'Pendiente',
-    autorizado = 'Autorizado',
-    no_autorizado = 'No autorizado',
+enum Enum_ProjectPhase {
+    iniciado = "Iniciado",
+    en_desarrollo = "En desarrollo",
+    terminado= "Terminado",
+    nula=''
+} 
+
+
+enum Enum_StatusProject {
+    activo = "Activo",
+    inactivo = "Inactivo",
+
 }
-enum Enum_EstadoProyecto {
-    activo = 'Activo',
-    inactivo = 'Inactivo',
+
+enum Enum_TypeObjetive {
+    general = "General",
+    especifico = "Específico",
+
 }
-enum Enum_FaseProyecto {
-    iniciado = 'Iniciado',
-    desarrollo = 'En desarrollo',
-    terminado = 'Terminado',
-    nula = '',
+
+enum Enum_StatusIncription {
+    aceptada = "Aceptada",
+    rechazada = "Rechazada",
+
 }
-enum Enum_TipoObjetivo{
-    general  = 'General',
-    especifico = 'Específico',
-}
-enum Enum_EstadoInscripcion{
-    acepatada ='Aceptada',
-    rechazada = 'Rechazada',
-}
-export { Enum_Rol, Enum_EstadoUsuario, Enum_EstadoProyecto, Enum_FaseProyecto, Enum_TipoObjetivo, Enum_EstadoInscripcion }
+
+
+
+export {Enum_Rol, Enum_StatusUsers, Enum_StatusProject, Enum_ProjectPhase, Enum_TypeObjetive, Enum_StatusIncription}
